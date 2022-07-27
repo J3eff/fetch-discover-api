8813eb7516db459f19763484ddcb7b444999f449
J3eff <jeffersonvideo125@gmail.com>
2022-07-26 22:07:47 -0300

    GET com paramentros

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-const url="http://localhost:5500/api"
+const url = "http://localhost:5500/api";
 
-function getUsers(){
-    fetch(url)
-    .then(response => response.json())
-    .then(data => renderApiResult.textContent = JSON.stringify(data))
-    .catch(error => console.error(error))
+function getUsers() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => (renderApiResult.textContent = JSON.stringify(data)))
+    .catch((error) => console.error(error));
+}
+
+function getUser(id) {
+  fetch(`${url}/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+        userName.textContent = data.name;
+        userCity.textContent = data.city;
+        userAvatar.src = data.avatar;
+    })
+    .catch((error) => console.error(error))
 }
 
 getUsers()
+getUser(1)
